80573dcc642d6c722cdff772d9fd413ff6ccdd3b
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-22 17:53:15 -0400

    feat:dto statistic
    Adiciona classe StatisticDto com propriedades essenciais
    
    Foi adicionada a nova classe `StatisticDto` no namespace
    `BasqueteCansadoApi.Dto`. A classe contém as seguintes
    propriedades: `Id` (Guid), `CategoryId` (Guid), `MatchId`
    (Guid), `PlayerId` (Guid), `Team` (bool) e `CreatedAt`
    (DateTime).

---
+++ Dto/StatisticDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BasqueteCansadoApi.Dto
+{
+    public class StatisticDto
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid MatchId { get; set; }
+        public Guid PlayerId { get; set; }
+        public bool Team { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
