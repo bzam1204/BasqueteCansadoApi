25e45f11d72f12ed2316fc8676b88c73768fcd88
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-22 17:53:40 -0400

    Adiciona rotas CRUD para StatisticCategory
    Foram adicionadas novas rotas no arquivo `StatisticCategoryRoutes.cs` para a entidade `StatisticCategory`, incluindo as operações de deletar, obter todas, obter por id e criar uma nova categoria de estatística. Também foram adicionados os `usings` necessários para o funcionamento das rotas.

---
+++ Routes/StatisticCategoryRoutes.cs
@@ -0,0 +1,55 @@
+﻿using BasqueteCansadoApi.Data;
+using BasqueteCansadoApi.Dto;
+using BasqueteCansadoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasqueteCansadoApi.Routes
+{
+    public static class StatisticCategoryRoutes
+    {
+        public static void MapStatisticCategoryRoutes(this IEndpointRouteBuilder endpoins)
+        {
+            //delete a statistic category
+            endpoins.MapDelete("/statisticCategories/{id}", async (AppDbContext context, Guid id) =>
+            {
+                var statisticCategory = await context.StatisticCategories.FindAsync(id);
+                if (statisticCategory is null) return Results.NotFound();
+                context.StatisticCategories.Remove(statisticCategory);
+                await context.SaveChangesAsync();
+                return Results.NoContent();
+            });
+            //get all statistic categories
+            endpoins.MapGet("/statisticCategories", async (AppDbContext context) =>
+            {
+                var statisticCategories = await context.StatisticCategories.ToListAsync();
+                return Results.Ok(statisticCategories);
+            });
+            //get a statistic category by id
+            endpoins.MapGet("/statisticCategories/{id}", async (AppDbContext context, Guid id) =>
+            {
+                var statisticCategory = await context.StatisticCategories.FindAsync(id);
+                return statisticCategory is null ? Results.NotFound() : Results.Ok(statisticCategory);
+            });
+            //create a new statistic category
+            endpoins.MapPost("/statisticCategories", async (AppDbContext context, string name) =>
+            {
+                var newStatisticCategory = new StatisticCategory
+                {
+                    Id = Guid.NewGuid(),
+                    Name = name
+                };
+                context.StatisticCategories.Add(newStatisticCategory);
+                await context.SaveChangesAsync();
+
+                var statisticCategoryDto = new StatisticCategoryDto
+                {
+                    Id = newStatisticCategory.Id,
+                    Name = newStatisticCategory.Name
+                };
+
+                return Results.Created($"/statisticCategories/{statisticCategoryDto.Id}", statisticCategoryDto);
+            });
+
+        }
+    }
+}
